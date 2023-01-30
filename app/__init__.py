425a2ccfc4f56339d6762fccbcf188fd6e20f822
Robert <robertvandevlasakker@outlook.com>
2023-01-30 10:20:28 +0100

    removed vendor files

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,13 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from flask_bootstrap import Bootstrap5
 from config import Config
+from flask_assets import Environment, Bundle
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 bootstrap = Bootstrap5(app)
+assets = Environment(app)
 
 # Blueprint Registration
 from app.main import bp as main_bp
@@ -17,16 +20,22 @@ from app.errors import bp as errors_bp
 app.register_blueprint(main_bp)
 app.register_blueprint(errors_bp)
 
+# Asset Registration
+assets.url = app.static_url_path
+scss = Bundle('src/scss/theme.scss', 
+                filters='libsass', output='css/all.css')
+assets.register('scss_all', scss) 
 
-if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/website.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Website startup')
+# if not app.debug:
+#     if not os.path.exists('logs'):
+#         os.mkdir('logs')
+#     file_handler = RotatingFileHandler('logs/website.log', maxBytes=10240,
+#                                        backupCount=10)
+#     file_handler.setFormatter(logging.Formatter(
+#         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+#     file_handler.setLevel(logging.INFO)
+#     app.logger.addHandler(file_handler)
+
+#     app.logger.setLevel(logging.INFO)
+#     app.logger.info('Website startup')
